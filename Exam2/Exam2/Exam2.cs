e1f2ef0d013bf3096215ebacb3085a36a51a55a5
HannuMoller <moller.hannu@gmail.com>
2017-02-14 15:10:44 +0200

    first two exams

---
+++ Exam2/Exam2/Exam2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using exam;
+
+namespace Exam2
+{
+    class Exam2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter number : ");
+            string snum = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(snum);
+                if (Number.isEven(number))
+                {
+                    Console.WriteLine("{0} is even", number);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is odd", number);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} was not number!", snum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam2/Exam2/Number.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace exam
+{
+
+    /// <summary>
+    /// Class Number contains static methods for checking numer properties
+    /// </summary>
+    public class Number
+    {
+
+        public const int NEGATIVE = -1;
+        public const int ZERO = 0;
+        public const int POSITIVE = 1;
+
+        public Number()
+        {
+        }
+
+        /// <summary>
+        /// check if number is either negative, positive or zero
+        /// </summary>
+        /// <param name="number"> number to be inspected </param>
+        /// <returns> Negative | Zero | Positive </returns>
+        public static int check(int number)
+        {
+            if (number < 0)
+            {
+                return NEGATIVE;
+            }
+            else if (number > 0)
+            {
+                return POSITIVE;
+            }
+            else
+            {
+                return ZERO;
+            }
+        }
+
+        /// <summary>
+        /// Check if number is even
+        /// </summary>
+        /// <param name="number"> number to be inspected </param>
+        /// <returns> true, if number is even </returns>
+        public static bool isEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+    }
+}
