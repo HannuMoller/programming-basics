b7269c0a52dfabbedb1649b778dc6fe19f982bcc
HannuMoller <moller.hannu@gmail.com>
2017-02-15 10:34:44 +0200

    Exam5 added

---
+++ Exam5/Exam5/Customer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam5
+{
+    /// <summary>
+    /// Customer data and logic for calculating price discounts
+    /// </summary>
+    class Customer
+    {
+        /// <summary>
+        /// base price (without discounts) for customer
+        /// </summary>
+        const float BASEPRICE = 16.00f;
+
+        String name = null;
+        int age = 0;
+        bool isStudent = false;
+        bool isConscript = false;
+        bool isMTKmember = false;
+
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void setStudent(bool isStudent)
+        {
+            this.isStudent = isStudent;
+        }
+
+        public void setConscript(bool isConscript)
+        {
+            this.isConscript = isConscript;
+        }
+
+        public void setMTKmember(bool isMTKmember)
+        {
+            this.isMTKmember = isMTKmember;
+        }
+
+        /// <summary>
+        /// calculate actual price for customer
+        /// </summary>
+        /// <returns> actual price with discounts </returns>
+        public float getPrice()
+        {
+            float price = BASEPRICE;
+
+            if (age < 7)
+            {
+                price = 0.00f; // free
+            }
+            else if (age >= 65)
+            {
+                price *= 0.50f; // 50% discount
+            }
+            else if (age <= 15)
+            {
+                price *= 0.50f; // 50% discount
+            }
+            else if (isConscript)
+            {
+                price *= 0.50f; // 50% discount
+            }
+            else
+            {
+                if (isStudent)
+                {
+                    price *= 0.55f; // 45% discount
+                }
+                if (isMTKmember)
+                {
+                    price *= 0.85f; // 15% discount
+                }
+            }
+
+            return price;
+        }
+
+    }
+}
---
+++ Exam5/Exam5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter customer data...");
+            Console.Write("Enter name : ");
+            string name = Console.ReadLine();
+            Customer customer = new Customer(name);
+            int age = -1;
+            while (age < 0)
+            {
+                Console.Write("Enter age : ");
+                if (!int.TryParse(Console.ReadLine(), out age)) 
+                {
+                    Console.WriteLine("Invalid age!");
+                    age = -1;
+                }
+                else {
+                    customer.setAge(age);
+                }
+            }
+            customer.setMTKmember(YesNo("is customer member of MTK"));
+            customer.setConscript(YesNo("is customer a conscript"));
+            customer.setStudent(YesNo("is customer a student"));
+
+            Console.WriteLine("Price for {0} is {1:F2}", name, customer.getPrice());
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Accepts only Y or N as answer (ok, lowercase also accepted ;)
+        /// </summary>
+        /// <param name="prompt"> the question </param>
+        /// <returns> true, if Y | false, if N </returns>
+        private static bool YesNo(string prompt)
+        {
+            while (true)
+            {
+                Console.Write("{0} (Y/N) : ", prompt);
+                char c = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+                if (c == 'Y')
+                {
+                    return true;
+                }
+                if (c == 'N')
+                {
+                    return false;
+                }
+                Console.WriteLine("Please answer Y or N!");
+            }
+        }
+    }
+}
