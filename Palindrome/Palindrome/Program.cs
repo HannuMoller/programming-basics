9b578c4966a5ccc9953f23633ad358c69bbd46d1
HannuMoller <moller.hannu@gmail.com>
2017-02-18 09:32:54 +0200

    project Palindrome added

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    /// <summary>
+    /// read string, check if it is a palindrome
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome checker v1.0");
+            Console.Write("Enter string : ");
+            String pal = Console.ReadLine();
+            if (isPalindrome(pal))
+            {
+                Console.WriteLine("- is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("- is not a palindrome!");
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// inspect if given string is a palindrome
+        /// </summary>
+        /// <param name="s"> string to be inspected </param>
+        /// <returns> true, if given string is a palindrome </returns>
+        static Boolean isPalindrome(string s)
+        {
+            s = removeExtra(s);
+
+            int i = 0;
+            int j = s.Length - 1;
+            while (j > i)
+            {
+                if (s.ElementAt(i) != s.ElementAt(j))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// remove extra characters, leaving only letters
+        /// and letters will be uppercased
+        /// </summary>
+        /// <param name="s"> original string </param>
+        /// <returns> cleaned up version of string </returns>
+        static string removeExtra(string s)
+        {
+            string s2 = "";
+            int i = 0;
+            while (i < s.Length)
+            {
+                char c = s.ElementAt(i);
+                if (char.IsLetter(c))
+                {
+                    s2 += char.ToUpper(c);
+                }
+                i++;
+            }
+
+            return s2;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Programming basics exams
 
-1. Exam 1:
+1. Exam1:
 	- read number, evaluate if it is either negative, zero or positive
-2. Exam 2:
+2. Exam2:
 	- read number, evaluate if it is either even or odd
-3. Exam 3:
+3. Exam3:
 	- combine exams 1 and 2
-4. Exam 4:
+4. Exam4:
 	- read 3 numbers, sort them in ascending order
-5. Exam 5:
+5. Exam5:
 	- calculate ticket price according to customer's data and discount rules
+
+6. Palindrome:
+	- read string, evaluate if it is a palindrome
